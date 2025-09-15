888e6b74565d1dbd6636f99ff791a62f0f44a8f5
Nikita Karasev <karasev.nikita1605@yandex.ru>
2025-09-15 11:37:06 +0300

    Этап 2 тестовый пуш 6

--- ewm-main-service/src/main/java/ru/practicum/ewm/main/controller/admin/AdminCategoriesController.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/main/controller/admin/AdminCategoriesController.java
@@ -1,6 +1,7 @@
 package ru.practicum.ewm.main.controller.admin;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.main.dto.CategoryDto;
@@ -18,9 +19,15 @@ public class AdminCategoriesController {
 
     private final CategoryService categoryService;
 
+//    @PostMapping
+//    public CategoryDto create(@RequestBody
+//@Valid NewCategoryDto dto) {
+//        return categoryService.create(dto);
+//    }
+
     @PostMapping
-    public CategoryDto create(@RequestBody
-@Valid NewCategoryDto dto) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDto create(@Valid @RequestBody NewCategoryDto dto) {
         return categoryService.create(dto);
     }
 
@@ -31,9 +38,15 @@ public class AdminCategoriesController {
         return categoryService.update(catId, dto);
     }
 
+//    @DeleteMapping("/{catId}")
+//    public void delete(@PathVariable
+//@Min(1) long catId) {
+//        categoryService.delete(catId);
+//    }
+
     @DeleteMapping("/{catId}")
-    public void delete(@PathVariable
-@Min(1) long catId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long catId) {
         categoryService.delete(catId);
     }
 }
