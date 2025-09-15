c40fdc5339c970b85b1787fb20f3753ec9d83bde
Nikita Karasev <karasev.nikita1605@yandex.ru>
2025-09-15 19:49:48 +0300

    Этап 2 тестовый пуш 13

--- ewm-main-service/src/main/java/ru/practicum/ewm/main/mapper/RequestMapper.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/main/mapper/RequestMapper.java
@@ -14,7 +14,7 @@ public interface RequestMapper {
 
     @Mapping(target = "event", source = "event.id")
     @Mapping(target = "requester", source = "requester.id")
-    @Mapping(target = "created", source = "created", qualifiedByName = "fmt")
+    @Mapping(target = "created", source = "created", qualifiedByName = "formatIso")
     @Mapping(target = "status", source = "status")
     ParticipationRequestDto toDto(ParticipationRequest pr);
 
@@ -23,5 +23,5 @@ public interface RequestMapper {
         return ldt == null ? null
                 : ldt.format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
-    
+
 }
